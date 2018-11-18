cd3f59bfe79b159698832f22b093b0ca67dcc04e
Ryo Midorikawa <midoriiiiii9866@gmail.com>
2018-11-18 16:46:16 +0900

    init commit

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -4,7 +4,7 @@ class BlogsController < ApplicationController
   def index
     @blogs = Blog.all
   end
-  
+
   def new
     if params[:back]
       @blog = Blog.new(blog_params)
@@ -12,7 +12,7 @@ class BlogsController < ApplicationController
       @blog = Blog.new
     end
   end
-  
+
   def create
     @blog = Blog.create(blog_params)
     if @blog.save
@@ -21,15 +21,15 @@ class BlogsController < ApplicationController
       render 'new'
     end
   end
-  
+
   def show
     @blog = Blog.find(params[:id])
   end
-  
+
   def edit
     @blog = Blog.find(params[:id])
   end
-  
+
   def update
     @blog = Blog.find(params[:id])
     if @blog.update(blog_params)
@@ -38,26 +38,29 @@ class BlogsController < ApplicationController
       render 'edit'
     end
   end
-  
+
   def destroy
     @blog.destroy
     redirect_to blogs_path, notice:"ブログを削除しました！"
   end
-    
+
   def confirm
     @blog = Blog.new(blog_params)
     render :new if @blog.invalid?
   end
-  
-  private 
-  
+
+  def top
+  end
+
+  private
+
   def blog_params
     params.require(:blog).permit(:title, :content)
   end
-  
+
   def set_blog
     @blog = Blog.find(params[:id])
   end
 
-  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'blogs/top' ,to: 'blogs#top'
   resources :blogs do
     collection do
       post :confirm
     end
   end
+
 end
