a5229486a6c071cdb660993b05c2709806a3291e
Ryo Midorikawa <midoriiiiii9866@gmail.com>
2018-12-01 12:37:20 +0900

    fix mailer

--- app/mailers/blog_mailer.rb
+++ app/mailers/blog_mailer.rb
@@ -2,6 +2,6 @@ class BlogMailer < ApplicationMailer
   def blog_mail(blog)
     @blog = blog
 
-    mail to: "@blog.user.email", subject: "お問い合わせの確認メール"
+    mail to: @blog.user.email, subject: "お問い合わせの確認メール"
   end
 end
