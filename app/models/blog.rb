3ee25fd6051a653fef0f6be7b76c3ba805cc4290
Ryo Midorikawa <midoriiiiii9866@gmail.com>
2018-12-06 07:37:01 +0900

    add image walidates

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,6 @@
 class Blog < ApplicationRecord
   validates :title, presence: true
+  validates :image, presence: true
   validates :title,:content, length: { in: 1..140 }
   belongs_to :user
   has_many :favorites, dependent: :destroy
